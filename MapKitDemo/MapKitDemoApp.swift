a86b14ea85921fc572b0655b7589d9491d838dad
Aoli <939677612@qq.com>
2024-04-16 15:53:45 -0500

    Initial Commit

---
+++ MapKitDemo/MapKitDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapKitDemoApp.swift
+//  MapKitDemo
+//
+//  Created by Shaohua Zhang on 4/16/24.
+//
+
+import SwiftUI
+
+@main
+struct MapKitDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
